9b8ac577db9a5a9be49d5c9137c2ce88e80666e2
Perry MacMurray <perrymacmurray@gmail.com>
2020-07-20 19:31:03 -0400

    Added ore generation Added RandomCollection class for use with ore generation

---
+++ src/main/java/com/protania/orechanger/util/RandomCollection.java
@@ -0,0 +1,110 @@
+package com.protania.orechanger.util;
+
+import net.minecraft.util.Tuple;
+
+import java.util.*;
+
+public class RandomCollection<T> {
+    private class WeightTuple<T> extends Tuple<T, Integer> {
+        public WeightTuple(T object, int weight) {
+            super(object, weight);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o instanceof WeightTuple) {
+                return this == o;
+            } else {
+                return this.get().equals(o);
+            }
+        }
+
+        public int getWeight() {
+            return this.getB();
+        }
+
+        public T get() {
+            return this.getA();
+        }
+    }
+
+    private final List<WeightTuple<T>> list = new ArrayList<>();
+    private final Random rand;
+    private int total = 0;
+
+    public RandomCollection(Random random) {
+        rand = random;
+    }
+
+    public boolean contains(T o) {
+        return list.contains(o);
+    }
+
+    public Iterator<T> getIterator() {
+        List<T> result = new ArrayList<T>();
+        for (WeightTuple t : list) {
+            result.add((T)t.get());
+        }
+        return result.iterator();
+    }
+
+    public boolean add(int weight, T o) {
+        if (list.contains(o))
+            return false;
+        total += weight;
+        return list.add(new WeightTuple<>(o, weight));
+    }
+
+    public boolean addAll(int weight, Collection<T> collection) {
+        for (T o : collection) {
+            add(weight, o);
+        }
+        return true;
+    }
+
+    public boolean remove(T o) {
+        boolean result = false;
+        for (WeightTuple listObject : list) {
+            if (listObject.get().equals(o)) {
+                result = list.remove(listObject);
+                if (result)
+                    total -= listObject.getWeight();
+                break;
+            }
+        }
+        return result;
+    }
+
+    private WeightTuple getRandomWeightTuple() {
+        int ticket = rand.nextInt(total);
+        int cumulativeWeight = 0;
+        for (WeightTuple listObject : list) {
+            cumulativeWeight += listObject.getWeight();
+            if (cumulativeWeight >= ticket) {
+                return listObject;
+            }
+        }
+
+        return null; //This should never EVER happen
+    }
+
+    public T getRandom() {
+        return (T) getRandomWeightTuple().get();
+    }
+
+    public T getRandomAndRemove() {
+        WeightTuple selection = getRandomWeightTuple();
+        if (list.remove(selection))
+            total -= selection.getWeight();
+
+        return (T) selection.get();
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public int weightedSize() {
+        return total;
+    }
+}
--- src/main/java/com/protania/orechanger/worldgen/CustomVeinFeature.java
+++ src/main/java/com/protania/orechanger/worldgen/CustomVeinFeature.java
@@ -1,24 +1,98 @@
 package com.protania.orechanger.worldgen;
 
+import com.protania.orechanger.OreChanger;
+import com.protania.orechanger.config.data.BlockInformation;
 import com.protania.orechanger.config.data.CustomVeinInfo;
+import com.protania.orechanger.util.RandomCollection;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ISeedReader;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.structure.StructureManager;
+import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.Random;
+import java.util.*;
 
 public class CustomVeinFeature extends Feature<CustomVeinFeatureConfig> {
     public CustomVeinFeature() {
         super(CustomVeinFeatureConfig.codec);
     }
 
+    private static BlockPos moveDirection(BlockPos origin, int i) {
+        BlockPos result;
+        switch(i) {
+            case 0:
+                result = origin.down();
+                break;
+            case 1:
+                result = origin.up();
+                break;
+            case 2:
+                result = origin.north();
+                break;
+            case 3:
+                result = origin.east();
+                break;
+            case 4:
+                result = origin.south();
+                break;
+            case 5:
+                result = origin.west();
+                break;
+            default:
+                result = null;
+        }
+        return result;
+    }
+
     @Override
-    public boolean func_230362_a_(ISeedReader worldIn, StructureManager structureManager, ChunkGenerator chunkGenerator, Random rand, BlockPos blockPos, CustomVeinFeatureConfig featureConfig) {
+    public boolean func_230362_a_(ISeedReader worldIn, StructureManager structureManager, ChunkGenerator chunkGenerator, Random rand, BlockPos chunkBlockPos, CustomVeinFeatureConfig featureConfig) {
         CustomVeinInfo cvi = featureConfig.info;
 
-        //Do things here
+        for (int veinNum = 0; veinNum < cvi.getNumPerChunk(); veinNum++) {
+            RandomCollection<BlockPos> blocks = new RandomCollection<>(rand);
+
+            BlockPos veinOrigin = new BlockPos(chunkBlockPos).add(rand.nextInt(16), rand.nextInt((cvi.getYLevelMax() - cvi.getSize()) - (cvi.getYLevelMin() + cvi.getSize())) + (cvi.getYLevelMin() + cvi.getSize()), rand.nextInt(16));
+            blocks.add(cvi.getRadius(), veinOrigin);
+
+            for (int i = 0; i < cvi.getSize(); i++) {
+                veinOrigin = moveDirection(veinOrigin, rand.nextInt(6));
+                if (blocks.contains(veinOrigin)) {
+                    i--;
+                } else {
+                    blocks.add(cvi.getRadius(), veinOrigin);
+                }
+            }
+
+            for (int pass = 1; pass < cvi.getRadius(); pass++) {
+                List<BlockPos> discoveredBlocks = new ArrayList<BlockPos>();
+                for (Iterator<BlockPos> it = blocks.getIterator(); it.hasNext(); ) {
+                    BlockPos b = it.next();
+                    for (int i = 0; i < 6; i++) {
+                        BlockPos neighbor = moveDirection(b, i);
+                        if (!blocks.contains(neighbor) && !discoveredBlocks.contains(neighbor)) {
+                            if (neighbor.getY() > 0 && neighbor.getY() <= 255)
+                                discoveredBlocks.add(neighbor);
+                        }
+                    }
+                }
+
+                blocks.addAll(cvi.getRadius() - pass, discoveredBlocks);
+            }
+
+            for (BlockInformation blockInfo : cvi.getBlockInformation()) {
+                for (int i = (rand.nextInt(blockInfo.getMaxAmount() - blockInfo.getMinAmount()) + blockInfo.getMinAmount()); i > 0; i--) {
+                    BlockPos selection = blocks.getRandomAndRemove();
+                    BlockState currentState = worldIn.getBlockState(selection);
+                    //TODO implement target blocks (make default stone)
+                    if (currentState.isSolid()) { //Not air or fluid
+                        worldIn.setBlockState(selection, ForgeRegistries.BLOCKS.getValue(blockInfo.getBlock()).getDefaultState(), 3);
+                    }
+                }
+            }
+        }
 
         return true;
     }
